30770369aeb4f45b6ab917a696091b09912d3da9
Saulo Andrade <sauloandradegames@gmail.com>
2023-10-10 06:52:45 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# java-runewords-api
+
+Java RESTful API for saving information about runewords in Diablo 2 and related games. Developed during DIO's Santander Bootcamp 2023 FullStack Java+Angular.
---
+++ src/test/java/edu/sauloandrade/runewordsapi/RunewordsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sauloandrade.runewordsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunewordsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
